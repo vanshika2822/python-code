0bdc943650ad9aa03a8a2cf512c56599ed52b0f1
vanshika2822 <64768251+vanshika2822@users.noreply.github.com>
2022-07-24 14:05:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "\
+[general]\n\
+email=\"21f1001757@student.onlinedegree.iitm.ac.in\"\n
+" > ~/.streamlit/config.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
